9852d3a922b2a86bc8149e94695d7229436c65a2
RavindraSinghSamant <ravindrasamant009@gmail.com>
2025-10-28 15:29:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple scroll animation
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
